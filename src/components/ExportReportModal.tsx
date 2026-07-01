697c03538975fb9845f60f9b0f03cd536a1031b1
chhunsan2015-prog <chhunsan2015@gmail.com>
2026-07-01 16:43:48 +0700

    style: increase font size for branch names in report

--- src/components/ExportReportModal.tsx
+++ src/components/ExportReportModal.tsx
@@ -257,7 +257,7 @@ export default function ExportReportModal({
 
         // Column A: Branch Name
         setCell(0, rowIndex, b.nameKh, {
-          font: { sz: 9.5, bold: true, color: { rgb: '0F172A' } },
+          font: { sz: 11, bold: true, color: { rgb: '0F172A' } },
           alignment: { horizontal: 'left', vertical: 'center', wrapText: true },
           border: tableBorder
         });
